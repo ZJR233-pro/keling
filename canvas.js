0d07ac2dacb997048050f8740bd3eb9d44104997
ZJR233-pro <1103525839@qq.com>
2026-03-18 17:35:51 +0800

    Refactor canvas drawing code for better readability

--- canvas.js
+++ canvas.js
@@ -1,17 +1,6 @@
-const canvas=document.getElementById('canvas');
-const ctx=canvas.getContext('2d');
-let scale=1;
-canvas.width=canvas.parentElement.clientWidth;
-canvas.height=canvas.parentElement.clientHeight;
-
-function draw(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.save();
-  ctx.scale(scale,scale);
-  ctx.fillStyle="#eee";
-  ctx.fillRect(100,100,400,400);
-  ctx.restore();
-}
+const canvas=document.getElementById('canvas');const ctx=canvas.getContext('2d');let scale=1;
+canvas.width=canvas.parentElement.clientWidth;canvas.height=canvas.parentElement.clientHeight;
+function draw(){ctx.clearRect(0,0,canvas.width,canvas.height);ctx.save();ctx.scale(scale,scale);ctx.fillStyle="#eee";ctx.fillRect(100,100,400,400);ctx.restore()}
 draw();
 document.getElementById('zoomIn').onclick=()=>{scale*=1.2;draw()}
 document.getElementById('zoomOut').onclick=()=>{scale/=1.2;draw()}
